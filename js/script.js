f4f8fdd21c204b19d19bfec88e2a5c004e627085
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-12-22 15:30:43 -0500

    restaurant template

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+videoBtn.forEach(btn =>{
+  btn.addEventListener('click', ()=>{
+      document.querySelector('.controls .active').classList.remove('active');
+      btn.classList.add('active');
+      let src = btn.getAttribute('data-src');
+      document.querySelector('#video-slider').src = src;
+  });
+});
